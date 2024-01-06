ba372ec2d4c9af0f97b445549245f86a99582c38
Precious <preshengr@gmail.com>
2024-01-06 09:28:48 +0100

    Header file created

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,18 +2,21 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for ALX project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
