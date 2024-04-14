fc25e0bf4c499a971aff3c4fd660583aed7e26c7
Precious <preshengr@gmail.com>
2024-04-14 17:58:39 +0100

    Update on task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const args = process.argv
-      .map(Number)
-      .slice(2, process.argv.length)
-      .sort((a, b) => a - b);
-    console.log(args[args.length - 2]);
-  }
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
+}
