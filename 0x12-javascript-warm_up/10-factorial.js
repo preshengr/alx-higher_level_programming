e51f0f6a13f3cb28833e55bcd3d58b617093ce2a
Presh Engr <preshengr@mail.com>
2024-04-14 17:49:58 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
 function factorial (n) {
-    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
-  }
-  
-  console.log(factorial(Number(process.argv[2])));
+  if (n === 0 || isNaN(n)) {
+      return 1;
+  } else {
+   return n * factorial(n - 1);
+ }
+}
+
+let firstArg = process.argv[2];
+firstArg = parseInt(firstArg);
+console.log(factorial(firstArg));
