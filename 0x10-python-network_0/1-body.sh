1b43695511ed99529188252e8fa835153021736d
preshengr <preshengr@mail.com>
2024-05-18 07:34:37 +0100

    Update on 1-body.sh File

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# Sends a GET request to the URL, and displays the body of the response
+curl -s -L "${1}"
